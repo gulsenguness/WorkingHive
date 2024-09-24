df4c430cd6e0d0afe08d84f22f99825ac3a635d3
Gülşen Güneş <gulsenurgunes@gmail.com>
2024-09-24 21:00:46 +0300

    structure has been made, arrangements will be made

--- app/src/main/java/com/gulsengunes/workinghive/ui/ChampionTasksFragment.kt
+++ app/src/main/java/com/gulsengunes/workinghive/ui/ChampionTasksFragment.kt
@@ -14,7 +14,6 @@ class ChampionTasksFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_champion_tasks, container, false)
     }
 
