7dadacffffe1a88db506092aa11e70910bea5771
Gülşen Güneş <gulsenurgunes@gmail.com>
2024-10-02 00:43:55 +0300

    lottie library added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,7 @@ android {
 }
 
 dependencies {
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -59,5 +60,7 @@ dependencies {
     kapt(libs.hilt.android.compiler)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
+
+    implementation (libs.lottie)
 
 }
